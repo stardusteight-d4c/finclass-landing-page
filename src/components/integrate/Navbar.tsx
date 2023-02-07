6c1294733727f9f7a4c69fc916df47718c7e07fc
Gabriel Sena <stardusteight.d4cc@gmail.com>
2023-02-07 20:25:58 -0300

    handle menu mobile done

--- src/components/integrate/Navbar.tsx
+++ src/components/integrate/Navbar.tsx
@@ -10,18 +10,44 @@ interface Props {}
 
 export const Navbar = (props: Props) => {
   const [openMenu, setOpenMenu] = useState<boolean>(false)
+  const headerRefElement = useRef<HTMLElement>(null)
   const minWith768px = useMediaQuery({
     query: '(min-width: 768px)',
   })
+  const [currentScrollPosition, setCurrentScrollPosition] = useState<number>(0)
 
-  document.body.classList.toggle('no-scroll', openMenu)
+  useEffect(() => {
+    if (minWith768px) {
+      setOpenMenu(false)
+    }
+  }, [minWith768px])
+
+  useEffect(() => {
+    if (openMenu) {
+      setCurrentScrollPosition(window.pageYOffset)
+      setTimeout(() => {
+        document.body.classList.add('no-scroll')
+      }, 800)
+      setTimeout(() => {
+        headerRefElement.current!.classList.remove('navGlassmorphism')
+        window.scrollTo(0, currentScrollPosition)
+      }, 1000)
+    } else {
+      document.body.classList.remove('no-scroll')
+      headerRefElement.current!.classList.add('navGlassmorphism')
+      window.scrollTo(0, currentScrollPosition)
+    }
+  }, [openMenu, headerRefElement])
 
   return (
     <>
       {/* In Firefox there are conflicts in rendering the menu because of the
     backdrop-filter property of navGlassmorphism */}
       <MobileMenu openMenu={openMenu} setOpenMenu={setOpenMenu} />
-      <header className="navGlassmorphism z-[500] fixed w-full max-w-screen py-4 md:py-7 mx-auto">
+      <header
+        ref={headerRefElement}
+        className="z-[500] fixed w-full max-w-screen py-4 md:py-7 mx-auto"
+      >
         <nav className="flex max-w-screen-xl mx-auto px-4 md:px-8 items-center justify-between">
           <img
             src={finclass}
@@ -46,7 +72,7 @@ export const Navbar = (props: Props) => {
             <img
               src={menu}
               alt="open-menu-hamburguer"
-              onClick={() => setOpenMenu(!openMenu)}
+              onClick={() => setOpenMenu(true)}
               className="block md:hidden cursor-pointer"
             />
           </div>
